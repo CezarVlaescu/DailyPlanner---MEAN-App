a279fb5fe21042d2e17470090d3316a98b3c505f
Vlaescu Cezar <vlaescucezar@yahoo.com>
2023-11-14 19:13:36 +0200

    3

---
+++ src/app/modules/homepage/components/home/task/task.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen'},
+  {position: 2, name: 'Helium'},
+  {position: 3, name: 'Lithium'},
+  {position: 4, name: 'Beryllium'},
+  {position: 5, name: 'Boron'},
+  {position: 6, name: 'Carbon'},
+  {position: 7, name: 'Nitrogen'},
+  {position: 8, name: 'Oxygen'},
+  {position: 9, name: 'Fluorine'},
+  {position: 10, name: 'Neon'},
+];
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
+})
+export class TaskComponent {
+  displayedColumns: string[] = ['position', 'name'];
+  dataSource = ELEMENT_DATA;
+}
